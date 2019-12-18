7d5e339776f5022283f062327768f0cbc1e05c04
十三妖 <181907667@qq.com>
2019-12-18 23:15:19 +0800

    fastjson

---
+++ javatest-json/src/main/java/com/thd/json/Person.java
@@ -0,0 +1,82 @@
+package com.thd.json;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * com.thd.json.Person
+ *
+ * @author: wanglei62
+ * @DATE: 2019/12/18 22:57
+ **/
+public class Person {
+    @JSONField(ordinal = 1)
+    private String name;
+    // 不进行序列化
+    @JSONField(serialize=false)
+    private String nickName;
+
+    //指定顺序 指定json属性名
+    @JSONField(ordinal = 4,name="myAge")
+    private Integer age;
+
+    // 指定日期格式
+    @JSONField(format="yyyyMMdd",ordinal = 2)
+    private Date birthday;
+
+    @JSONField(ordinal = 3)
+    private Timestamp createTime;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
--- javatest-json/src/main/java/com/thd/json/Test.java
+++ javatest-json/src/main/java/com/thd/json/Test.java
@@ -6,32 +6,74 @@
 
 package com.thd.json;
 
-import java.util.Date;
 
-import net.sf.json.JSONObject;
-import net.sf.json.JsonConfig;
+import com.alibaba.fastjson.JSON;
+
+import java.sql.Timestamp;
+import java.util.*;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		JsonConfig jsonConfig = new JsonConfig();  
-		jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
-		 
-		JsonPerson jp = new JsonPerson();
-		jp.setAge(4);
-		jp.setBir(new Date());
-		jp.setName("张三");;
-		JSONObject jObj = JSONObject.fromObject(jp,jsonConfig);
-		System.out.println(jObj.toString());
-		
-		
-		String jsonStr = "{'age':4,'bir':'2014-06-25','name':'张三'}";
-		JSONObject jObj2 = JSONObject.fromObject(jsonStr);
-		System.out.println(jObj2.get("bir"));
-		
-		JsonPerson jp2 = (JsonPerson)JSONObject.toBean(jObj2,JsonPerson.class);
-		System.out.println(jp2.getBir());
+		Test.testToJson();
+		Test.testParseObject();
+		Test.testListToJson();
+		Test.testMapToJson();
+		Test.testJsonToMap();
+	}
+
+	public static void testToJson(){
+		Person person = new Person();
+		person.setAge(5);
+		person.setName("devil13th");
+		person.setBirthday(new Date());
+		person.setCreateTime(new Timestamp(new Date().getTime()));
+		person.setNickName("十三妖");
+		String json = JSON.toJSONString(person);
+		System.out.println(json);
+	}
+
+	public static void testParseObject(){
+		String json = "{\"name\":\"devil13th\",\"birthday\":\"20191218\",\"createTime\":1576681508433,\"myAge\":5,\"nickName\":\"十三妖\"}";
+		Person p = JSON.parseObject(json,Person.class);
+		System.out.println(p.toString());
+	}
+
+
+	public static void testListToJson(){
+		List<Person> l = new ArrayList<Person>();
+		for(int i = 0 , j = 3 ; i < j ; i++){
+			Person person = new Person();
+			person.setAge(5);
+			person.setName("devil13th");
+			person.setBirthday(new Date());
+			person.setCreateTime(new Timestamp(new Date().getTime()));
+			person.setNickName("十三妖");
+			l.add(person);
+		}
+
+
+
+		String json = JSON.toJSONString(l);
+		System.out.println(json);
+	}
+
+
+	public static void testMapToJson(){
+		Map m = new HashMap();
+		m.put("name","devil13th");
+		m.put("age",5);
+		m.put("birthday",new Date());
+		m.put("createTime",new Timestamp(new Date().getTime()));
+		String json = JSON.toJSONString(m);
+		System.out.println(json);
+	}
+
+
+	public static void testJsonToMap(){
+		String json = "{\"birthday\":1576681977915,\"createTime\":1576681977915,\"name\":\"devil13th\",\"age\":5}";
+		Map m = JSON.parseObject(json,Map.class);
+		System.out.println(m);
 	}
 
 }
